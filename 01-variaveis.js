7ff45dea4d49a7e7e674c9ae1560d6e834e78db8
anna <anajugeremias@gmail.com>
2024-02-07 11:28:03 -0300

    JaveScript_comandos basicos

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//Criando "variaveis" com "var" (var nome variavel = <valor>;)
+
+var nome = "Ana";
+var idade = 17;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log("Agora fiz aniversario!😍 Tenho", idade+1)
+
+//aumentado automagicamente +1 (autoincrementos)
+idade++//idade = idade+1;
+console.log("Agora fiz aniversario!😍 Tenho", idade)
---
+++ 02-aritimedico.js
@@ -0,0 +1,27 @@
+//ultilizando operadores aritimeticos com javaScriot
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois numero informados por você (var nome variavel =12)
+
+var x = 10;
+var y = 15;
+console.log("A soma entre", x, "e", y, "é:", x + y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("soma = ", soma);
+console.log("subtração = ", subtracao);
+console.log("multiplicação = ", multiplicacao);
+console.log("divisão = ", divisao);
+
+//Regras psrs nomear identificadores (var, const ou let)
+//Podem começar com A-Z, a-z, _ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20;
+const _minhaCostante = 3.1514;
+var $minhaVariavel = 11;
