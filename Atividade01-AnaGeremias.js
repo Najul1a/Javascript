03bd71a29259114203b5e3f901815b8d544d8aeb
anna <anajugeremias@gmail.com>
2024-02-07 17:03:54 -0300

    FinalizadoAtividadesdeaula

---
+++ Atividade01-AnaGeremias.js
@@ -0,0 +1,29 @@
+//Exercicio para fixação
+
+var nome = "Livia Ferreira de Lima Solla";
+var banco = "Santander";
+var agencia = 6362;
+var conta = 74376347377882899287;
+var saldo = 13000;
+
+console.log("Olá, seja bem vinda", nome, "a sua conta do", banco);
+console.log("tem o saldo de", saldo);
+console.log("com numero da agencia", agencia);
+console.log("e numero da conta", conta);
+console.log("saldo disponivel na sua conta", saldo);
+
+saldo += 200;
+console.log("voce recebeu um pix de: R$", 200);
+
+saldo -= 10;
+console.log("voce efetuou um pagamento na agua de coco do tio didi R$", 10);
+
+saldo += 600;
+console.log("jogo do trigrinho pagou voce no valor de R$", 600);
+
+saldo += 1000;
+console.log("Ana Geremias fez um pix de R$", 1000);
+
+saldo -= 874;
+console.log("voce efetuou um pagamento para força e luz", 874);
+console.log("seu saldo atual é de", saldo);
