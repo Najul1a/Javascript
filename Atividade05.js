2a138154c3fe1b6da1c4f3221e498d243fbc4dca
anna <anajugeremias@gmail.com>
2024-02-28 10:19:14 -0300

    Atividade05

---
+++ Atividade05.js
@@ -0,0 +1,14 @@
+// Atividade01
+function idade(ano1, ano2) {
+  return ano1 - ano2;
+}
+console.log(`${idade(2024, 2007)}`);
+
+console.log("--------------------------------------");
+//Atividade2
+function combustivel(litros, km) {
+  return litros * km;
+}
+console.log(
+  `Ele pode viajar com 40 litros de combustivel : ${combustivel(40, 12)} kms`
+);
